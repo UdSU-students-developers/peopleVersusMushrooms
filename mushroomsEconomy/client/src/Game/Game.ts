7a4f1b498e6b819b58981bff7808a3565dcbaea2
aleksandr813 <molokoflina@gmail.com>
2026-03-19 23:45:08 +0400

    new file:   mushroomsEconomy/client/src/Game/Game.ts

---
+++ mushroomsEconomy/client/src/Game/Game.ts
@@ -0,0 +1,7 @@
+import React, { Context } from "react"
+
+export default class Game {
+    constructor() {
+        this.server // Надо сменить app на конексты и дописать тут штуки для получения и обработки данных
+    }
+}

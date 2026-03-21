99a2f17624d77a555c9660c19f63d6e25c3264c7
moromov <nmoromov@vk.com>
2026-03-21 21:30:17 +0400

    PID_Moromov

--- mushroomsArmy/client/src/config.ts
+++ mushroomsArmy/client/src/config.ts
@@ -1,5 +1,19 @@
 const CONFIG = {
-    HOST: 'http://localhost:3008', // Адрес сервера
+    HOST: 'http://localhost:3002', // Адрес сервера
+
+    MEDIATOR: {
+        TRIGGERS: {
+            SET_STORE: 'SET_STORE',
+            GET_STORE: 'GET_STORE',
+            CLEAR_STORE: 'CLEAR_STORE',
+        }
+    },
+
+    SOCKET: {
+        REGISTRATION: 'REGISTRATION',
+        LOGIN: 'LOGIN',
+        LOGOUT: 'LOGOUT',
+    }
 };
 
 export default CONFIG;

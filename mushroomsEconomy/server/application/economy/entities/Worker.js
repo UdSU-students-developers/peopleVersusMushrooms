6313c6dffa4707fb6d334f6cc9f7a0e7cbac4106
golpgelop <golpgelop@gmail.com>
2026-03-22 21:10:57 +0400

    начал так уж писать

--- mushroomsEconomy/server/application/economy/entities/Worker.js
+++ mushroomsEconomy/server/application/economy/entities/Worker.js
@@ -1,5 +1,24 @@
-class Worker {
+const { HP, SPEED } = CONFIG.ECONOMY.WORKER;
 
+class Worker {
+    constructor({ x, y, guid, callbacks, easystar}) {
+        this.x = x;
+        this.y = y;
+        this.guid = guid;
+        this.callbacks = callbacks;
+        this.hp = HP;
+        this.speed = SPEED;
+        this.easystar = easystar;
+
+    }
+
+    get() {
+        return {
+            x: this.x,
+            y: this.y,
+            hp: this.hp
+        }
+    }
 }
 
 module.exports = Worker;

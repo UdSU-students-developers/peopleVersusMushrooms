b019d23ddcc71bd8a492ed22e3fd8b81bb74919f
golpgelop <golpgelop@gmail.com>
2026-03-22 14:00:07 +0400

    Апп исправил на норм

--- mushroomsEconomy/client/src/App.tsx
+++ mushroomsEconomy/client/src/App.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
-import CONFIG from './config';
+
 import Server from './services/Server/Server';
-import Store from './services/Store/Store';
-import useStore from './services/Store/useStore';
 import Mediator from './services/Mediator/Mediator';
-import useMediator from './services/Mediator/useMediator';
 
 import PageManager from './pages/PageManager';
+import Popup from './components/Popup/Popup';
+
+import useMediator from './services/Mediator/useMediator';
+import useStore from './services/Store/useStore';
 
 import './App.css';
+
 
 export const MediatorContext = React.createContext<Mediator>(null!);
 export const ServerContext = React.createContext<Server>(null!);
 
-const { MEDIATOR } = CONFIG;
+function App() {
 
-const App: React.FC = () => {
-    const mediator = useMediator();
-    useStore(mediator);
-    const server = new Server(mediator);
+  const mediator = useMediator();
+  useStore(mediator);
+  const server = new Server(mediator);
 
-    const pressMeHandler = () => mediator.get(
-        MEDIATOR.TRIGGERS.MESSAGE,
-        { name: 'Vasya', text: 'something' }
-    );
 
-    return (
-        <MediatorContext value={mediator}>
-            <ServerContext value={server}>
-            <div className="App">
-                <button onClick={pressMeHandler}>Press Me</button>
-                <div className='app'>
-                    <PageManager />
-                </div>
-            </div>
-            </ServerContext>
-        </MediatorContext>
-    );
+  return (
+    <div className="App">
+      <MediatorContext.Provider value={mediator}>
+        <ServerContext.Provider value={server}>
+          <div className='app'>
+            <PageManager />
+            <Popup />
+          </div>
+        </ServerContext.Provider>
+      </MediatorContext.Provider>
+    </div>
+  );
 }
 
 export default App;

c4facce7942f304e85229ff71907617717788306
Smetannn <egorsmetanin2006@gmail.com>
2026-03-25 23:12:48 +0400

    add page army to pagemanager.tsx

--- peopleArmy/client/src/pages/PageManager.tsx
+++ peopleArmy/client/src/pages/PageManager.tsx
@@ -4,6 +4,7 @@ import Registration from './Registration/Registration';
 import Login from './Login/Login';
 import Chat from './Chat/Chat';
 import Store from '../services/Store/Store';
+import Army from './Army/Army';
 import Mediator from '../services/Mediator/Mediator';
 import CONFIG from '../config';
 import { MediatorContext, ServerContext } from '../App';
@@ -12,6 +13,7 @@ export enum PAGES {
     LOGIN,
     REGISTRATION,
     CHAT,
+    ARMY,
 }
 
 export interface IBasePage {
@@ -37,6 +39,7 @@ const PageManager: React.FC = () => {
             {page === PAGES.REGISTRATION && <Registration {...props} />}
             {page === PAGES.LOGIN && <Login {...props} />}
             {page === PAGES.CHAT && <Chat {...props} />}
+            {page === PAGES.ARMY && <Army {...props} />}
         </>
     );
 }

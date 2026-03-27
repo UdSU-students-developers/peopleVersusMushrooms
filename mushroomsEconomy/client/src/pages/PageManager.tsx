9419a6ab4b723d572c094acb7be5bd89e6cdbe54
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-03-27 23:00:52 +0400

    modified:   mushroomsEconomy/client/src/pages/PageManager.tsx

--- mushroomsEconomy/client/src/pages/PageManager.tsx
+++ mushroomsEconomy/client/src/pages/PageManager.tsx
@@ -19,7 +19,7 @@ export interface IBasePage {
 }
 
 const PageManager: React.FC = () => {
-    const [page, setPage] = useState<PAGES>(PAGES.LOGIN);
+    const [page, setPage] = useState<PAGES>(PAGES.GAME);
 
     const props = {
         setPage,

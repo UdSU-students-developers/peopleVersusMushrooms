69b62bfa7288e790c506669d80b2f07765498203
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-03-29 12:20:22 +0400

    modified:   mushroomsEconomy/server/application/modules/lobby/LobbyManager.js

--- mushroomsEconomy/server/application/modules/lobby/LobbyManager.js
+++ mushroomsEconomy/server/application/modules/lobby/LobbyManager.js
@@ -2,13 +2,15 @@ const CONFIG = require('../../../config');
 const BaseManager = require('../BaseManager');
 const Lobby = require('./Lobby');
 
+const { SOCKET } = CONFIG;
+
 class LobbyManager extends BaseManager {
     constructor(options) {
         super(options);
 
         this.users = {};
-        this.lobbies = {}; 
-        this.userToRoom = {}; 
+        this.lobbies = {};
+        this.userToLobby = {};
 
         this.init();
     }
@@ -21,156 +23,136 @@ class LobbyManager extends BaseManager {
         io.on('connection', (socket) => {
             console.log(`LobbyManager: Client connected: ${socket.id}`);
 
-            socket.on(CONFIG.SOCKET.CREATE_ROOM, (data) => this.handleCreateRoom(socket, data));
-            socket.on(CONFIG.SOCKET.JOIN_ROOM, (data) => this.handleJoinRoom(socket, data));
-            socket.on(CONFIG.SOCKET.LEAVE_ROOM, (data) => this.handleLeaveRoom(socket, data));
-            socket.on(CONFIG.SOCKET.DROP_FROM_ROOM, (data) => this.handleKickUser(socket, data));
-            socket.on(CONFIG.SOCKET.GET_ROOMS, (data) => this.handleGetRooms(socket, data));
-            
+            socket.on(SOCKET.CREATE_LOBBY, (data) => this.socketCreateLobby(socket, data));
+            socket.on(SOCKET.JOIN_LOBBY,   (data) => this.socketJoinLobby(socket, data));
+            socket.on(SOCKET.LEAVE_LOBBY,  (data) => this.socketLeaveLobby(socket, data));
+            socket.on(SOCKET.KICK_FROM_LOBBY, (data) => this.socketKickFromLobby(socket, data));
+            socket.on(SOCKET.GET_LOBBIES,  (data) => this.socketGetLobbies(socket, data));
+
             socket.on('disconnect', () => this.handleDisconnect(socket));
         });
     }
-    
-    sendError(socket, code) {
-        const message = this.answer.bad(code);
-        socket.emit(CONFIG.SOCKET.ERROR, { code, message });
-    }
 
-    getUser(socket) { 
+    getUser(socket) {
         return this.users[socket.id];
     }
 
-    handleCreateRoom(socket, data) {
+    socketCreateLobby(socket, data) {
         const user = this.getUser(socket);
-        if (!user) return this.sendError(socket, 17);
-        if (!data || !data.name) return this.sendError(socket, 18);
+        if (!user) return this.answer.bad(17);
+        if (!data?.name) return this.answer.bad(18);
 
-        if (this.userToRoom[user.id]) {
-            this._leaveRoom(user.id, socket);
+        if (this.userToLobby[user.id]) {
+            this._leaveLobby(user.id, socket);
         }
 
-        const lobby = new Lobby({ 
-            creatorGuid: user.id, 
-            roomName: data.name, 
-            common: this.common 
+        const lobby = new Lobby({
+            creatorGuid: user.id,
+            roomName: data.name,
+            common: this.common
         });
 
         this.lobbies[lobby.guid] = lobby;
-        this.userToRoom[user.id] = lobby.guid;
+        this.userToLobby[user.id] = lobby.guid;
 
         socket.join(lobby.guid);
-
-        socket.emit('room_created', lobby.get());
-        this._notifyRoomsListUpdated();
+        socket.emit(SOCKET.LOBBY_CREATED, lobby.get());
+        this._notifyLobbiesUpdated();
     }
 
-    handleJoinRoom(socket, data) {
+    socketJoinLobby(socket, data) {
         const user = this.getUser(socket);
-        if (!user) return this.sendError(socket, 17);
-        
-        const { roomId } = data;
-        const lobby = this.lobbies[roomId];
-        
-        if (!lobby) return this.sendError(socket, 19);
+        if (!user) return this.answer.bad(17);
 
+        const lobby = this.lobbies[data?.lobbyId];
+        if (!lobby) return this.answer.bad(19);
         if (lobby.isGuidInRoom(user.id)) return;
 
-        if (this.userToRoom[user.id]) {
-            this._leaveRoom(user.id, socket);
+        if (this.userToLobby[user.id]) {
+            this._leaveLobby(user.id, socket);
         }
 
         if (lobby.addPlayer(user.id)) {
-            this.userToRoom[user.id] = lobby.guid;
-            
+            this.userToLobby[user.id] = lobby.guid;
             socket.join(lobby.guid);
-
-            this._notifyRoomUpdate(lobby.guid);
-            this._notifyRoomsListUpdated();
+            this._notifyLobbyUpdate(lobby.guid);
+            this._notifyLobbiesUpdated();
         }
     }
 
-    handleLeaveRoom(socket, data) {
+    socketLeaveLobby(socket, data) {
         const user = this.getUser(socket);
-        if (!user) return this.sendError(socket, 17);
+        if (!user) return this.answer.bad(17);
 
-        this._leaveRoom(user.id);
-        socket.emit('room_updated', { status: 'left' });
+        this._leaveLobby(user.id, socket);
+        socket.emit(SOCKET.LOBBY_UPDATED, { status: 'left' });
     }
 
-    handleKickUser(socket, data) {
+    socketKickFromLobby(socket, data) {
         const admin = this.getUser(socket);
-        if (!admin) return this.sendError(socket, 17);
+        if (!admin) return this.answer.bad(17);
 
-        const { targetGuid, roomId, userID } = data;
-        const lobby = this.lobbies[roomId];
+        const lobby = this.lobbies[data?.lobbyId];
+        if (!lobby) return this.answer.bad(19);
+        if (lobby.creatorGuid !== admin.id) return this.answer.bad(20);
 
-        if (!lobby) return this.sendError(socket, 19);
-        if (lobby.creatorGuid !== admin.id) return this.sendError(socket, 20);
-
-        if (lobby.removePlayer(targetGuid)) {
-            delete this.userToRoom[targetGuid];
-            this._notifyRoomUpdate(lobby.guid);
+        if (lobby.removePlayer(data.targetGuid)) {
+            delete this.userToLobby[data.targetGuid];
+            this._notifyLobbyUpdate(lobby.guid);
         }
     }
 
-    handleGetRooms(socket, data) {
-        const rooms = this._getPublicRooms();
-        socket.emit('rooms_list', rooms);
+    socketGetLobbies(socket, data) {
+        socket.emit(SOCKET.LOBBIES_LIST, this._getLobbies());
     }
 
     handleDisconnect(socket) {
         const user = this.getUser(socket);
         if (user) {
-            this._leaveRoom(user.id, socket);
+            this._leaveLobby(user.id, socket);
             delete this.users[socket.id];
         }
     }
 
-    _leaveRoom(userGuid, socket) {
-        const roomGuid = this.userToRoom[userGuid];
-        if (!roomGuid) return;
+    _leaveLobby(userGuid, socket) {
+        const lobbyGuid = this.userToLobby[userGuid];
+        if (!lobbyGuid) return;
 
-        const lobby = this.lobbies[roomGuid];
+        const lobby = this.lobbies[lobbyGuid];
         if (!lobby) {
-            delete this.userToRoom[userGuid];
+            delete this.userToLobby[userGuid];
             return;
         }
 
         lobby.removePlayer(userGuid);
-        delete this.userToRoom[userGuid];
+        delete this.userToLobby[userGuid];
 
-        if (socket) {
-            socket.leave(roomGuid);
-        }
+        if (socket) socket.leave(lobbyGuid);
 
         if (userGuid === lobby.creatorGuid || lobby.players.size === 0) {
             this._destroyLobby(lobby);
         } else {
-            this._notifyRoomUpdate(roomGuid);
+            this._notifyLobbyUpdate(lobbyGuid);
         }
     }
 
     _destroyLobby(lobby) {
         delete this.lobbies[lobby.guid];
-        this._notifyRoomsListUpdated();
+        this._notifyLobbiesUpdated();
     }
 
-    _getPublicRooms() {
+    _getLobbies() {
         return Object.values(this.lobbies).map(l => l.get());
     }
 
-    _notifyRoomUpdate(roomGuid) {
-        const lobby = this.lobbies[roomGuid];
+    _notifyLobbyUpdate(lobbyGuid) {
+        const lobby = this.lobbies[lobbyGuid];
         if (!lobby) return;
-
-        const data = lobby.get();
-        
-        this.io.to(roomGuid).emit('room_updated', data);
+        this.io.to(lobbyGuid).emit(SOCKET.LOBBY_UPDATED, lobby.get());
     }
 
-    _notifyRoomsListUpdated() {
-        const rooms = this._getPublicRooms();
-        this.io.emit('rooms_list_updated', rooms);
+    _notifyLobbiesUpdated() {
+        this.io.emit(SOCKET.LOBBIES_LIST_UPDATED, this._getLobbies());
     }
 }
 

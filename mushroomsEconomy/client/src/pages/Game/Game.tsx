a463d6f4565239c493b1e1f871a87ab402513175
ArtemOL <artemolin19@gmail.com>
2026-03-30 19:43:44 +0400

    Сделал меню как страницу

--- mushroomsEconomy/client/src/pages/Game/Game.tsx
+++ mushroomsEconomy/client/src/pages/Game/Game.tsx
@@ -1,99 +1,35 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { IBasePage, PAGES } from '../PageManager';
 import GameCanvas from './GameCanvas';
 import Button from '../../components/Button/Button';
-import CONFIG from '../../config';
-import "./Game.css";
-import { MediatorContext, ServerContext } from '../../App';
-import { useEffect } from 'react';
 
-interface IUser {
-    name: string;
-    token: string;
-    guid: string;
-}
+import "./Game.css"
+import { MediatorContext, ServerContext } from '../../App';
+
+const GAME_FIELD = 'game-field';
 
 const Game: React.FC<IBasePage> = (props: IBasePage) => {
     const { setPage } = props;
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [userInfo, setUserInfo] = useState<IUser | null>(null);
 
-    const mediator = useContext(MediatorContext);
-    const { GET_STORE } = CONFIG.MEDIATOR.TRIGGERS;
-
-    useEffect(() => {
-        if (mediator) {
-            const user = mediator.get<IUser | null>(GET_STORE, 'user');
-            if (user) {
-                setUserInfo(user);
-            }
-        }
-    }, [mediator]);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    const handleResume = () => {
-        setIsMenuOpen(false);
-    };
-
-    const handleGoToChat = () => {
-        setIsMenuOpen(false);
-        setPage(PAGES.CHAT); 
+    const handleOpenMenu = () => {
+        setPage(PAGES.GAME_MENU);
     };
 
     return (
-        <div className='game'>
-            <div>
+    <div className='game'>
+        <div>
                 <GameCanvas />
-            </div>
+        </div>
 
-            <div className="game-menu-btn-container">
+        <div className="game-menu-btn-container">
                 <Button 
-                    onClick={toggleMenu} 
+                    onClick={handleOpenMenu} 
                     text="Меню" 
                     className="menu-toggle-btn"
                 />
             </div>
-
-            {isMenuOpen && (
-                <div className="game-menu-overlay">
-                    <div className="game-menu-content">
-                        <h2>Меню</h2>
-                        <div className="player-info-block">
-                            {userInfo ? (
-                                <>
-                                    <div className="info-row">
-                                        <span className="info-label">Игрок:</span>
-                                        <span className="info-value">{userInfo.name}</span>
-                                    </div>
-                                    <div className="info-row">
-                                        <span className="info-label">Socket ID:</span>
-                                        <span className="info-value socket-id">{userInfo.guid}</span>
-                                    </div>
-                                </>
-                            ) : (
-                                <div className="info-loading">Загрузка данных...</div>
-                            )}
-                        </div>
-                        <div className="game-menu-buttons">
-                            <Button 
-                                onClick={handleResume} 
-                                text="Вернуться в игру" 
-                                className="button button-primary"
-                            />
-                            <Button 
-                                onClick={handleGoToChat} 
-                                text="Чат" 
-                                className="button button-secondary"
-                            />
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+    </div>
+);
 };
 
 export default Game;

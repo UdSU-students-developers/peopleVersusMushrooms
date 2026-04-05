2d76e5ebc2e47113c7ec071579b801ec999c64c0
ArtemOL <artemolin19@gmail.com>
2026-04-05 14:52:54 +0400

    Внёс изменения в Map

--- mushroomsEconomy/server/application/modules/map/Map.js
+++ mushroomsEconomy/server/application/modules/map/Map.js
@@ -1,33 +1,40 @@
-const BaseManager = require('../BaseManager');
-
-class MapManager extends BaseManager {
-    constructor(options) {
-        super(options);
+class Map { 
+    constructor() {
         this.map = null;
     }
 
     generate(width = 50, height = 50) {
         this.map = [];
-
         for (let y = 0; y < height; y++) {
             const row = [];
             for (let x = 0; x < width; x++) {
-                let tile = 0;
-
-                const rand = Math.random();
-                
-                if (rand > 0.95) {
-                    tile = 2; 
-                } else if (rand > 0.90) {
-                    tile = 1;
-                }
-
-                row.push(tile);
+                row.push(0);
             }
             this.map.push(row);
         }
 
-        console.log(`[MapManager] Карта ${width}x${height} сгенерирована.`);
+        const spotsCount = Math.floor(Math.random() * 10) + 5;
+        const maxRadius = 10;
+
+        for (let i = 0; i < spotsCount; i++) {
+            const cx = Math.floor(Math.random() * width);
+            const cy = Math.floor(Math.random() * height);
+
+            const type = Math.random() > 0.5 ? 1 : 2;
+
+            const currentRadius = Math.floor(Math.random() * (maxRadius - 3)) + 3;
+
+            for (let y = cy - currentRadius; y <= cy + currentRadius; y++) {
+                for (let x = cx - currentRadius; x <= cx + currentRadius; x++) {
+                    if (x >= 0 && x < width && y >= 0 && y < height) {
+                        const dist = Math.sqrt((x - cx) ** 2 + (y - cy) ** 2)
+                        if (dist <= currentRadius) {
+                            this.map[y][x] = type;
+                        }
+                    }
+                }
+            }
+        }
         return this.map;
     }
 
@@ -36,4 +43,4 @@ class MapManager extends BaseManager {
     }
 }
 
-module.exports = MapManager;
+module.exports = Map;

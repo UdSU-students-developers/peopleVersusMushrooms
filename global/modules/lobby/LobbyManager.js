581381fff774bd0788903914fc28b429c6733036
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-04-13 18:09:39 +0400

    modified:   global/modules/db/data.db 	modified:   global/modules/lobby/LobbyManager.js

--- global/modules/lobby/LobbyManager.js
+++ global/modules/lobby/LobbyManager.js
@@ -10,7 +10,7 @@ const { SOCKET } = GLOBAL_CONFIG;
 class LobbyManager extends BaseManager {
     constructor({ options, role }) { //Роль берите из своего конфига, она совпадает с названием вашего сервиса, например 'mushroomEconomy'
         super(options);
-
+        
         this.role = role;
 
 		if (!this.io) return;
@@ -49,37 +49,79 @@ class LobbyManager extends BaseManager {
         const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
         if (user) {
             this.sendToMap('/joinToLobby', { 
-                guid, 
-                lobbyGuid, 
+                guid: guid, 
+                lobbyGuid: lobbyGuid, 
                 role: this.role, 
             });
             return;
         }
-        socket.emit(SOCKET.JOIN_TO_LOBBY, this.answer.bad(9000));
+        socket.emit(SOCKET.JOIN_TO_LOBBY, this.answer.bad(1001));
     }
     
     socketCreateLobby(data, socket) {
+        const { guid, lobbyName } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/createLobby', {
+                guid: guid,
+                lobbyName: lobbyName,
+                role: this.role,
+            });
+            return;
+        } 
+        socket.emit(SOCKET.CREATE_LOBBY, this.answer.bad(1001));
+
 
     }
     
-    socketLeaveLobby (data, socket) {
-        
+    socketLeaveLobby(data, socket) {
+        const { guid } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/leaveLobby', { guid });
+            return;
+        }
+        socket.emit(SOCKET.LEAVE_LOBBY, this.answer.bad(1001));
     }
-
-    socketDropFromLobby (data, socket) {
-        
+    
+    socketDropFromLobby(data, socket) {
+        const { guid, targetGuid } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/dropFromLobby', { guid, targetGuid });
+            return;
+        }
+        socket.emit(SOCKET.DROP_FROM_LOBBY, this.answer.bad(1001));
     }
-
-    socketStartGame (data, socket) {
-        
+    
+    socketStartGame(data, socket) {
+        const { guid } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/startGame', { guid });
+            return;
+        }
+        socket.emit(SOCKET.START_GAME, this.answer.bad(1001));
     }
-
-    socketGetLobbies (data, socket) {
-        
+    
+    socketGetLobbies(data, socket) {
+        const { guid } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/getLobbies', { guid });
+            return;
+        }
+        socket.emit(SOCKET.GET_LOBBIES, this.answer.bad(1001));
     }
-
-    socketSetReady (data, socket) {
-
+    
+    socketSetReady(data, socket) {
+        const { guid } = data;
+        const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
+        if (user) {
+            this.sendToMap('/setReady', { guid });
+            return;
+        }
+        socket.emit(SOCKET.SET_READY, this.answer.bad(1001));
     }
 
     

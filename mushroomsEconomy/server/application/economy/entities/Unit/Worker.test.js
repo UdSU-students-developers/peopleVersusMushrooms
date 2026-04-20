42d0a83fa8c2c5043d0277decac222e3f356c8b4
golpgelop <golpgelop@gmail.com>
2026-04-21 01:35:55 +0400

    modified:   mushroomsEconomy/server/application/economy/entities/Unit/Worker.test.js

--- mushroomsEconomy/server/application/economy/entities/Unit/Worker.test.js
+++ mushroomsEconomy/server/application/economy/entities/Unit/Worker.test.js
@@ -11,7 +11,7 @@ describe("Worker", () => {
 
     test("Необходимо создать работника с правильным SPEED", () => {
         const worker = new Worker({});
-        expect(worker.speed).toBe(1234);
+        expect(worker.speed).toBe(12345);
     });
     //Дописать!!!!!!!!!
 });

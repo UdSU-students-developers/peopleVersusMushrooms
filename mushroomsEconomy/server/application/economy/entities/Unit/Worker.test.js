ef54a432dca35e07acfa719013615c7e59e0e06d
golpgelop <golpgelop@gmail.com>
2026-04-21 02:37:01 +0400

    Проверка на комит с ошибко

--- mushroomsEconomy/server/application/economy/entities/Unit/Worker.test.js
+++ mushroomsEconomy/server/application/economy/entities/Unit/Worker.test.js
@@ -11,7 +11,7 @@ describe("Worker", () => {
 
     test("Необходимо создать работника с правильным SPEED", () => {
         const worker = new Worker({});
-        expect(worker.speed).toBe(SPEED);
+        expect(worker.speed).toBe(123);
     });
     //Дописать!!!!!!!!!
 });

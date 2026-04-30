298a60cbffde538f57c2915c099709d7f869936f
sterio0o <ilyanaumov203@gmail.com>
2026-04-30 15:15:52 +0400

    Добавил тест для useGetReliefHandler

--- mushroomsEconomy/server/application/router/handlers/game/useGetReliefHandler.js
+++ mushroomsEconomy/server/application/router/handlers/game/useGetReliefHandler.js
@@ -2,7 +2,7 @@ module.exports = (mediator, answer) => {
     return (req, res) => {
         const { mapGuid, userGuid } = req.body;
         if (!mapGuid) {
-            return res.send(answer.bad(1004));
+            return res.send(answer.bad(3002));
         }
         const { GET_RELIEF_HANDLER } = mediator.getTriggerTypes();
 		const relief = mediator.get(GET_RELIEF_HANDLER, { mapGuid, userGuid });

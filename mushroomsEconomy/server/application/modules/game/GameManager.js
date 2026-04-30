5521104e13ac68dabe36bda63d030dc2a68869c7
aleksandr813 <molokoflina@gmail.com>
2026-04-30 23:57:39 +0400

    Ivt 57 send unit (#256)
    * modified:   global/Answer.js
    	modified:   global/globalConfig.js
    	modified:   global/modules/db/mushroomsEconomy.db
    	modified:   mushroomsEconomy/server/application/economy/Economy.js
    	modified:   mushroomsEconomy/server/application/modules/game/GameManager.js
    	modified:   mushroomsEconomy/server/application/router/handlers/game/useStartGameHandler.js
    	modified:   mushroomsEconomy/server/package-lock.json
    	modified:   mushroomsEconomy/server/package.json
    
    * modified:   global/modules/db/mushroomsArmy.db
    	modified:   global/modules/db/mushroomsEconomy.db
    	modified:   map/client/src/pages/Lobby/Lobby.tsx
    	modified:   map/server/application/modules/db/map.db
    	modified:   mushroomsEconomy/server/application/economy/Economy.js
    	modified:   mushroomsEconomy/server/application/economy/entities/Buildings/SmallReactor.js
    	modified:   mushroomsEconomy/server/application/modules/game/GameManager.js
    	modified:   mushroomsEconomy/server/application/router/handlers/game/useStartGameHandler.js
    	modified:   peopleArmy/server/application/modules/db/data.db
    
    * modified:   global/modules/db/mushroomsArmy.db
    	modified:   global/modules/db/mushroomsEconomy.db
    	modified:   map/server/application/modules/db/map.db
    	modified:   mushroomsEconomy/server/application/economy/Economy.js
    
    * modified:   mushroomsEconomy/server/application/modules/game/GameManager.js
    
    * modified:   map/client/src/pages/Lobby/Lobby.tsx

--- mushroomsEconomy/server/application/modules/game/GameManager.js
+++ mushroomsEconomy/server/application/modules/game/GameManager.js
@@ -21,7 +21,6 @@ class GameManager extends BaseManager {
 		this.mediator.subscribe(this.EVENTS.START_GAME, (data) => this.eventStartGame(data));
 		this.mediator.subscribe(this.EVENTS.LOAD_GAME, (data) => this.eventLoadGame(data));
 		// mediator triggers setters
-		this.mediator.set(this.TRIGGERS.SET_SERIVCES_GUIDS, (guids) => this.triggerSetServicesGuids(guids));
 		//...
 	}
 
@@ -49,15 +48,14 @@ class GameManager extends BaseManager {
 
 	/* TRIGGERS */
 
-	triggerSetServicesGuids(guids = {}, data) {
-		if (guids) {
-			this.economies[data.guid].initGuids(guids);
-		}
-	}
 
 	/* EVENTS */
 	eventStartGame(data = {}) {
+
 		const { guids, startPoint } = data;
+		//console.log(guids);
+		//console.log(SET_SERVICES_GUIDS);
+
 		if (guids?.mushroomsEconomy) {
 			const guid = guids.mushroomsEconomy;
 			const user = this.mediator.get(this.TRIGGERS.GET_USER_BY_GUID, guid);
@@ -69,7 +67,6 @@ class GameManager extends BaseManager {
 						updated: (data) => this.callbackUpdate(guid, data),
 						spawnArmyUnit: (data) => this.spawnArmyUnit(data),
 					},
-					guid,
 					guids, 
 					startPoint
 				});
@@ -77,7 +74,7 @@ class GameManager extends BaseManager {
 					GLOBAL_CONFIG.SOCKET.START_GAME,
 					this.answer.good(this.economies[guid].get())
 				);
-				console.log("Экдономика созана");
+				console.log("Экономика создана");
 				return this.answer.good(true);
 			}
 			return this.answer.bad(1001)

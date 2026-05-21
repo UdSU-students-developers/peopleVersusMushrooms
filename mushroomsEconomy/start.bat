7cb9212f5f5707c1680e2d4f724facd09a207aff
golpgelop <golpgelop@gmail.com>
2026-05-22 00:37:24 +0400

    самую малость

--- mushroomsEconomy/start.bat
+++ mushroomsEconomy/start.bat
@@ -1,4 +1,3 @@
 @echo off
 start cmd /k "cd client && npm start"
-start cmd /k "cd server && npm run dev:watch"
-echo Both client and server are starting in separate windows.
+start cmd /k "cd server && node app.js"

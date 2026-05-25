1288842b108a552c764d2e2fdf2a99fb743f97ac
Alex <alexmacs938@gmail.com>
2026-05-25 07:08:07 +0400

    Синхронизация зданий с картой

--- mushroomsArmy/server/application/modules/army/ArmyManager.ts
+++ mushroomsArmy/server/application/modules/army/ArmyManager.ts
@@ -187,8 +187,6 @@ class ArmyManager extends BaseManager {
         const army = this.army[guid];
         if (!army) return;
 
-        const ownBuildings = army.buildings.map(building => building.getState());
-
         const unitEntities = army.buildMapUnitUpdateEntities();
         if (unitEntities.length > 0) {
             await this.send<{ mapGuid: string; userGuid: string; entities: typeof unitEntities }>(
@@ -197,14 +195,12 @@ class ArmyManager extends BaseManager {
             );
         }
 
-        // Здания отправляем только новые (map использует toggle: повторная отправка удаляет с карты)
-        const newBuildings = ownBuildings.filter(b => !army.sentBuildingGuids.has(b.guid));
-        if (newBuildings.length > 0) {
-            await this.send<{ mapGuid: string; userGuid: string; entities: TArmyState['buildings'] }>(
+        const buildingEntities = army.buildMapBuildingUpdateEntities();
+        if (buildingEntities.length > 0) {
+            await this.send<{ mapGuid: string; userGuid: string; entities: typeof buildingEntities }>(
                 `${GLOBAL_CONFIG.MAP.URL}${GLOBAL_CONFIG.URLS.UPDATE_BUILDINGS}`,
-                { mapGuid: army.mapGuid, userGuid: army.guid, entities: newBuildings }
+                { mapGuid: army.mapGuid, userGuid: army.guid, entities: buildingEntities }
             );
-            newBuildings.forEach(b => army.sentBuildingGuids.add(b.guid));
         }
 
         // Получаем видимых врагов
